29b7c397f525bbe05e48ab4e81cb10167f4b2307
c1onw-le2 <sy41340@gmail.com>
2021-04-06 11:13:30 +0900

    bof_re

--- 4week_pwnable/bof2.c
+++ 4week_pwnable/bof2.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 
+
+void win(){
+	system("/bin/sh");
+}
 int main(){
 	
 	char buf[256];
     puts("Try? \n");
-	puts("Input : ");
-	gets(&buf);
+	puts("Input : \n");
+	gets(buf);
 	puts("BAAMMMM!");
 
 }
